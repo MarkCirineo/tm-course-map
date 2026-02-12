786bbfa71bc889c1771d354c553c163251a9fd34
markcirineo <markcirineo22@gmail.com>
2026-02-11 19:05:28 -0500

    Fix auth logic using id

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,6 +38,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+      async session({ session, token }) {
+      if (session.user && token.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
     async redirect({ url, baseUrl }) {
       if (url.startsWith("/")) return `${baseUrl}${url}`;
       if (new URL(url).origin === baseUrl) return url;
